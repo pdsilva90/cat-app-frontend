d63ea80723d597a3571205f4f5dff87db9116de0
pdsilva90 <pdsilva90@gmail.com>
2023-05-19 15:28:06 -0500

    update api call

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,27 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client';
+
+import styles from './page.module.css';
+import { useState, useEffect } from 'react';
 
 export default function Home() {
+  const [cats, setCats] = useState([]);
+  useEffect(() => {
+    const getCats = async () => {
+      const response = await fetch('https://cat-app-back-end.herokuapp.com/cats')
+      const returnedCats = await response.json()
+      setCats(returnedCats);
+    }
+    getCats();
+  }, []);
+
   return (
     <main className={styles.main}>
       <h1> Hello React </h1>
+      {
+        cats.map(cat => (
+          <h2 key={ cat._id }> { cat.name }</h2>
+        ))
+      }
     </main>
   )
 }
